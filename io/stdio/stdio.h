c5246c91d242f9ecf4d3d6b6970108ba6c14de9e
Jenperdsmonlat1 <65119790+Jenperdsmonlat1@users.noreply.github.com>
2023-04-18 22:26:56 +0200

    Delete io directory

--- io/stdio/stdio.h
+++
@@ -1,9 +0,0 @@
-/* Contient toutes les fonctions utiles permettant 
-d'obtenir les entrées utilisateurs et afficher du texte et autre.*/
-
-#include <stddef.h>
-#include <stdint.h>
-
-// Permet de déterminer la longueur d'une chaîne de caractère à afficher.
-size_t strlen(const char *string);
-void print(char *string);
