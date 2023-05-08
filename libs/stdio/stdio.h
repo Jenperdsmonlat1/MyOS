9147b95e1c184c9d35129a5faeb464e1c71f8eaf
Jenperdsmonlat1 <65119790+Jenperdsmonlat1@users.noreply.github.com>
2023-05-08 16:11:22 +0200

    Delete libs directory

--- libs/stdio/stdio.h
+++
@@ -1,9 +0,0 @@
-/* Contient toutes les fonctions utiles permettant 
-d'obtenir les entrées utilisateurs et afficher du texte et autre.*/
-
-#include <stdint.h>
-
-void printf(const char *string);
-void putchar(char character);
-char *itoa(int16_t number, char *dest, uint8_t base);
-void reverse(char *source, char *dest);
