1ac7015309ebdb250c7a1581cf8936762eef6eeb
Jenperdsmonlat1 <65119790+Jenperdsmonlat1@users.noreply.github.com>
2023-05-28 20:01:29 +0200

    Delete crash_test directory

--- crash_test/vesa/vesa.c
+++
@@ -1,7 +0,0 @@
-#include "vesa.h"
-#include <stdint.h>
-
-void draw_pixel(uint32_t framebuffer, uint32_t pitch, uint32_t bpp, uint32_t pos_x, uint32_t pos_y, uint32_t color) {
-
-	uint32_t pixel;
-}
